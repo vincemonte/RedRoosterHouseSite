2c6bb806405f3cf2219b1de265eaec98882e2c6b
vincemonte <vincentmonteleone99@gmail.com>
2020-12-25 21:09:35 -0500

    Adopted png images for mobile (for better load times). Moved the positions of some images to better illustrate the image.

--- redrooster/config.py
+++ redrooster/config.py
@@ -1,7 +1,8 @@
-import json
+'''import json
 
 with open('/etc/config.json') as config_file:
-	config = json.load(config_file)
+	config = json.load(config_file)'''
 
 class Config:
-	SECRET_KEY = config.get('SECRET_KEY')
+	#SECRET_KEY = config.get('SECRET_KEY')
+	SECRET_KEY = 'devtest90394lksl'
